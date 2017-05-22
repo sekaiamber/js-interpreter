29f6292509e0256d43aac8290d18a79c3a54ad37
锡炎 <xiyan.xxm@alibaba-inc.com>
2017-05-22 17:14:17 +0800

    feature. add option parser.

---
+++ src/combinators/optionParser.js
@@ -0,0 +1,21 @@
+/**
+ * 可选分析器
+ * 这个分析器接受一个分析器，分析时如果这个分析器有值，则返回，否则返回一个占位的result返回原来的pos。
+ * 这个分析器通常运用在某些可选语法中，比如`if else`，这个`else`可能是不存在的。
+ */
+
+import Parser from './parser';
+import Result from './result';
+
+export default class OptionParser extends Parser {
+  constructor(parser) {
+    super();
+    this.parser = parser;
+  }
+
+  parse(tokens, pos) {
+    const result = this.parser.parse(tokens, pos);
+    if (result) return result;
+    return new Result(null, pos);
+  }
+}
