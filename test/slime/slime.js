044a7be2b2128f7df7ad249d1b0ca22753f8d807
锡炎 <xiyan.xxm@alibaba-inc.com>
2017-07-17 23:09:35 +0800

    feature. add code test.

--- test/slime/slime.js
+++ test/slime/slime.js
@@ -4,6 +4,13 @@ import Slime, {
   SlimeLexer,
 } from './../../examples/slime';
 
+const code = `n = 5;
+p = 1;
+while n > 0 do
+  p = p * n;
+  n = n - 1;
+end`;
+
 describe('Slime test', () => {
   it('lexer test', (done) => {
     const lexer = new SlimeLexer();
@@ -17,6 +24,15 @@ describe('Slime test', () => {
     const result = slime.eval('a = 1');
     expect(result.result).to.equal(1);
     expect(result.env.a).to.equal(1);
+    done();
+  });
+
+  it('slime', (done) => {
+    const slime = new Slime();
+    const result = slime.eval(code);
+    const env = result.env;
+    expect(env.n).to.equal(0);
+    expect(env.p).to.equal(120);
     done();
   });
 });
